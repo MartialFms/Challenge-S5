99da19a8083b7afb9536e49ce8c4dfe6ad4043f5
EricM <101633226+EricMauler@users.noreply.github.com>
2022-03-24 10:59:54 +0100

    Square

--- src/fr/ldnr/entities/Square.java
+++ src/fr/ldnr/entities/Square.java
@@ -1,41 +1,38 @@
 package fr.ldnr.entities;
 
 public class Square extends Shape {
-	private int side;
+	private double side;
 	
 	
     
-    public Square  (int side, int x, int y) {
+    public Square  (double side, int x, int y) {
     	super(x,y);
     	this.setSide(side);
     }
       
-    public Square (int side, Point center) {
+    public Square (double side, Point center) {
     	super (center);
     	this.setSide(side);
 
 }
 
-	public int getSide() {
+	public double getSide() {
 		return side;
 	}
 
-	public void setSide(int side) {
+	public void setSide(double side) {
 		if(side<0) side = 1;
 		else this.side = side;
 	}
 
-	public int getY() {
-		return y;
-	}
-
 	public double area() {
-		return this.side = this.side;
+		return this.side * this.side;
 	}
 
 	public double perimeter () {
 		return 4 * side;
 	}
+	@Override
 public String toString() {
 	return "Square side : " + side + super.toString();
 }
