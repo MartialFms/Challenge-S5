198fe037ff0d21ff3004b3262857a3e4bcb3c524
Sebastien Allaire <sebastien.allaire.fms@gmail.com>
2022-03-24 10:33:35 +0100

    :sparkles: Ajout de l'interface ainsi que de la classe qui implémente l'interface

---
+++ src/fr/ldnr/entities/IGraphicImpl.java
@@ -0,0 +1,50 @@
+package fr.ldnr.entities;
+
+import java.util.HashMap;
+
+import fr.ldnr.entities.Interfaces.IGraphic;
+
+public class IGraphicImpl implements IGraphic {
+	
+	private HashMap<Integer, Shape> shapes; 
+
+	public IGraphicImpl() {
+		shapes = new HashMap<>();
+	}
+
+	@Override
+	public void addShape(int id, Shape shape) {
+		shapes.put(id, shape);
+	}
+
+	@Override
+	public void deleteShape(int id) {
+		shapes.remove(id);
+	}
+
+	@Override
+	public Shape getShapeById(int id) {
+		return shapes.get(id);
+	}
+
+	@Override
+	public void moveShape(int id, int x, int y) {
+		Shape shape = shapes.get(id);
+		shape.getCenter().setX();
+		shape.getCenter().setY();
+	}
+
+	@Override
+	public void drawShape() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayAll() {
+		shapes.entrySet().stream()
+			.forEach(shapes -> System.out.println(shapes.getValue()));
+		
+	}
+
+}
---
+++ src/fr/ldnr/entities/Interfaces/IGraphic.java
@@ -0,0 +1,14 @@
+package fr.ldnr.entities.Interfaces;
+
+import fr.ldnr.entities.Shape;
+
+public interface IGraphic {
+	
+	public void addShape(int id, Shape shape);
+	public void deleteShape(int id);
+	public Shape getShapeById(int id);
+	public void moveShape(int id, int x, int y);
+	public void drawShape();
+	public void displayAll();
+
+}
