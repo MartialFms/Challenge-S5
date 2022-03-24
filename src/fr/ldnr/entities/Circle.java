7da7b16d4efea46b6e0f7c0c91f9f3c580235840
EricM <101633226+EricMauler@users.noreply.github.com>
2022-03-24 11:12:37 +0100

    Circle

--- src/fr/ldnr/entities/Circle.java
+++ src/fr/ldnr/entities/Circle.java
@@ -1,40 +1,33 @@
 package fr.ldnr.entities;
 
 public class Circle extends Shape {
-    private int x;
-    private int y;
     private double radius;
     
     public Circle(double radius, int x, int y) {
+    super (x,y);
     setRadius(radius);
-    setX(x);
-    setY(y);
 }
 
-	public int getX() {
-		return x;
+	public Circle() {
+		super(1,1);
+		setRadius(1);
 	}
 
-	public void setX(int x) {
-		this.x = x;
+	public double area( ) {
+		return Math.PI * this.radius * this.radius;
 	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
+	
 	public double getRadius() {
 		return radius;
 	}
 
 	public void setRadius(double radius) {
-		if (radius< 0) radius = 1;
-		else this.radius= radius;
+		if (radius<0) radius = 1;
+		else
 		this.radius = radius;
 	}
-	
+	public String toString () {
+		return "Circle : radius = " + radius + " " + super.toString();
+	}
 }
+
